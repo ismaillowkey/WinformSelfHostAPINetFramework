43efd4bee8290585592b7403bb3479123628fa25
TOHO-09 <TOHO-09@TOHO-I5>
2021-03-18 14:43:45 +0700

    first commit

---
+++ WinformSelfHostAPINetFramework/Controllers/ValuesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using WinformSelfHostAPINetFramework.Model;
+
+namespace WinformSelfHostAPINetFramework.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var serviceResponse = new ServiceResponse<string>();
+            serviceResponse.Success = true;
+
+            string text = "";
+            Program.MainForm.Invoke(new Action(() =>
+            {
+                text = Program.MainForm.textBox1.Text;
+            }));
+            serviceResponse.Data = text;
+            return Ok(serviceResponse);
+        }
+
+        [HttpPost("{id}")]
+        public IActionResult Get(string id)
+        {
+            var serviceResponse = new ServiceResponse<string>();
+            serviceResponse.Success = true;
+
+            Program.MainForm.Invoke(new Action(() =>
+            {
+                Program.MainForm.textBox1.Text = id;
+            }));
+            serviceResponse.Data = id;
+            return Ok(serviceResponse);
+        }
+    }
+}
---
+++ WinformSelfHostAPINetFramework/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinformSelfHostAPINetFramework
+{
+    static class Program
+    {
+        public static Form1 MainForm { get; private set; }
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            CreateWebHostBuilder().Build().RunAsync();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            MainForm = new Form1();
+            Application.Run(MainForm);
+        }
+
+        static int portApi = 6666;
+        public static IWebHostBuilder CreateWebHostBuilder() =>
+            WebHost.CreateDefaultBuilder()
+                .UseUrls("http://0.0.0.0:" + portApi + "/")
+                .UseStartup<Startup>();
+    }
+}
